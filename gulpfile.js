ea645710272220214c2efcf33eec54036bd23bb1
EdG570 <cole570@hotmail.com>
2016-07-21 19:59:04 -0400

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    }
+  });
+});
+
+gulp.task('watch', ['browserSync'], function() {
+  gulp.watch('app/css/*.css', browserSync.reload);
+  gulp.watch('app/js/*.js', browserSync.reload);
+  gulp.watch('app/*.html', browserSync.reload);
+});
+
+gulp.task('useref', function() {
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'));
+});
+
+
+
+
+
+
